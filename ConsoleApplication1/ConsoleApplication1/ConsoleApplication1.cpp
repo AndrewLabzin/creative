0f889fdc6c4874d05f6bf6fa213db471480a5e19
андрей <michailm7583@gmail.com>
2021-03-20 11:57:59 +0300

    first commit

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+    int count;
+    int element;
+    int g = 0;
+    bool what = true;
+    cin >> count;
+    int* massive = new int[count];
+    int* massive2 = new int[count];
+
+    for (int i = 0; i < count; i++) {
+        cin >> element;
+        massive[i] = element;
+    }
+
+    for (int i = 0; i < count; i++) {
+        if (i < (count - 1)) {
+            for (int k = i + 1; k < count; k++) {
+                if (massive[i] <= massive[k]) {
+                    what = false;
+                    break;
+                }
+                else {
+                    what = true;
+                }
+            }
+            if (what == true) {
+                massive2[g] = massive[i];
+                g++;
+            }
+        }
+        else {
+            massive2[g] = massive[i];
+        }
+    }
+
+    for (int i = 0; i <= g; i++) {
+        cout << massive2[i] << " ";
+    }
+}
---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int c = 0;
+    int el = 0;
+    int sum = 0;
+    int a = 1;
+    int x;
+    cout << "Количество элементов: ";
+    cin >> c;
+
+    int* mass = new int[c];
+
+    cout << "Элементы: ";
+
+    for (int i = 0; i < c; i++)
+    {
+        cin >> el;
+        mass[i] = el;
+    }
+
+
+   for (int i = 0; i < c; i++) 
+   {
+        if (mass[i] > 0) 
+        {
+            sum += mass[i];
+            a *= mass[i];
+        }
+    }
+
+    for (int i = 0; i < c; i++) 
+    {
+        for (int j = 0; j < c; j++) 
+        {
+            if (abs(mass[j]) > abs(mass[i])) 
+            {
+                x = mass[i];
+                mass[i] = mass[j];
+                mass[j] = x;
+            }
+        }
+    }
+
+    cout << "Отформатированный массив: ";
+    for (int i = 0; i < c; i++) 
+    {
+        cout << mass[i] << " ";
+    }
+
+    cout << endl << "Сумма положительных элементов = " << sum << endl;
+    cout << endl << "Произведение положительных элементов = " << a << endl;
+}
